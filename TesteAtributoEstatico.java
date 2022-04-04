8da3703e0a68e6d00731c066353c6f2fe765d825
Hugo Silva <81167453+HugoSilva05@users.noreply.github.com>
2022-04-04 20:05:31 -0300

    Create TesteAtributoEstatico.java

---
+++ TesteAtributoEstatico.java
@@ -0,0 +1,18 @@
+class TesteAtributoEstatico{
+    
+    static int somaDois(int valor){
+        return valor + 2;
+    }
+    
+    static void somaTres(int valor){
+        System.out.println(valor + 3);
+    }
+    
+	public static void main(String[] args){
+	    int valor = 1;
+	    int valor2 = somaDois(valor);
+	    somaTres(valor);
+	    System.out.println(valor);
+	    System.out.println(valor2);
+	}
+}
